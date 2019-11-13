389369e9083448996aaa91c75ab1c99019977b10
ENGR 102 Review Solutions <54051780+ENGR102Review@users.noreply.github.com>
2019-11-13 17:02:30 -0600

    Create 3.py

---
+++ Algorithms/3.py
@@ -0,0 +1,12 @@
+# two parallel lists that contain name and age data
+names_list = ['Dave', 'John', 'Bob', 'Sylvester']
+age_list = [24, 52, 39, 67]
+
+# print a header
+print('Name'.ljust(15) + 'Age')
+
+# iterate through the lists
+for i in range(len(names_list)):
+    # get the name, left justify it by 15 spaces
+    # get the corresponding age, print it after the justified name
+    print(str(names_list[i]).ljust(15) + str(age_list[i]))
