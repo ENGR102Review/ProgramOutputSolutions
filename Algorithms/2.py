54e98d430cd5788b4c1b0d4c82a65fce980e9837
ENGR 102 Review Solutions <54051780+ENGR102Review@users.noreply.github.com>
2019-11-13 16:05:29 -0600

    Create 2.py

---
+++ Algorithms/2.py
@@ -0,0 +1,26 @@
+# interpolation between 2D cartesian points
+# visualize this example here: https://www.desmos.com/calculator/lkyi2y4kbx
+
+# tuples are a good way to represent point coordinates, since they cannot change
+# these are in the format (x, y)
+point_A = (20, 16)
+point_B = (24, 8)
+
+# now compute the slope of the line connecting the two points
+# (y2 - y1) / (x2 - x1)
+slope = (point_B[1] - point_A[1]) / (point_B[0] - point_A[0])
+
+# now do the interpolation at x = 22.5
+
+x_value_to_interpolate = 22.5
+
+# this finds the change in x between the interpolation point and the lower x-value of the two original points
+change_in_x = x_value_to_interpolate - point_A[0]
+
+# now multiply the change in x by the slope, to find the change in y
+delta_y = change_in_x * slope
+
+# add the original y of the first point
+y = delta_y + point_A[1]
+
+print('The interpolated value at x = %f is %f' % (x_value_to_interpolate, y))
