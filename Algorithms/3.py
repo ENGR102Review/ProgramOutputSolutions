47251b648fc58953159cc60013d4391cae753534
ENGR 102 Review Solutions <54051780+ENGR102Review@users.noreply.github.com>
2019-11-13 17:05:57 -0600

    Update 3.py

--- Algorithms/3.py
+++ Algorithms/3.py
@@ -10,3 +10,14 @@ for i in range(len(names_list)):
     # get the name, left justify it by 15 spaces
     # get the corresponding age, print it after the justified name
     print(str(names_list[i]).ljust(15) + str(age_list[i]))
+
+
+print()
+
+# alternatively, if you wanted to right-justify:
+print('Name'.rjust(15) + 'Age'.rjust(15))
+# iterate through the lists
+for i in range(len(names_list)):
+    # get the name, right justify it by 15 spaces
+    # get the corresponding age, right justify by 15 spaces
+    print(str(names_list[i]).rjust(15) + str(age_list[i]).rjust(15))
