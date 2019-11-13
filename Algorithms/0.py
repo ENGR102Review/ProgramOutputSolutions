1ea5a48db2b5d39843963aaed617aa978428007f
ENGR 102 Review Solutions <54051780+ENGR102Review@users.noreply.github.com>
2019-11-13 15:50:06 -0600

    Create 0.py

---
+++ Algorithms/0.py
@@ -0,0 +1,10 @@
+print('Hello world')
+
+my_bool = True
+
+if my_bool:
+    # use exit(0) to stop your Python program mid-run
+    exit(0)
+
+# this code will never be reached
+print('Python is great')
