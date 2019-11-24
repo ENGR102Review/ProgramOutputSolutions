463590e71751701f65a3bceb95c35dbbe9194aa7
ENGR 102 Review Solutions <54051780+ENGR102Review@users.noreply.github.com>
2019-11-23 19:42:22 -0600

    Create 5.py

---
+++ Algorithms/5.py
@@ -0,0 +1,16 @@
+# here is a list of numbers
+my_list = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+# if you wanted to go through the list backwards:
+
+# you could use the downward counting loop approach
+for i in range(len(my_list) - 1, -1, -1):
+    print(my_list[i])
+
+# or you could use the negative indexes approach
+for i in range(1, len(my_list) + 1):
+    print(my_list[-i])
+
+# or you could use python's built in reversed() function
+for element in reversed(my_list):
+    print(element)
